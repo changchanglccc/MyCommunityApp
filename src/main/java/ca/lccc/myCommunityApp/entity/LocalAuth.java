f69864b5130c6fa5227822bd51d809bb12a9d821
changchanglccc <jennifer06091992@gmail.com>
2024-03-20 15:17:17 -0400

    1.feat: create 10 entities classes

---
+++ src/main/java/ca/lccc/myCommunityApp/entity/LocalAuth.java
@@ -0,0 +1,64 @@
+package ca.lccc.myCommunityApp.entity;
+
+import javax.swing.*;
+import java.util.Date;
+
+/**
+ * 给本地用户
+ */
+public class LocalAuth {
+    private Long localAuthId;
+    private String username;
+    private String password;
+    private Date createTime;
+    private Date lastEditTime;
+    private PersonInfo personInfo;   // 个人信息和本地账户是一一映射关系
+
+    public Long getLocalAuthId() {
+        return localAuthId;
+    }
+
+    public void setLocalAuthId(Long localAuthId) {
+        this.localAuthId = localAuthId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
---
+++ src/main/java/ca/lccc/myCommunityApp/entity/Product.java
@@ -0,0 +1,143 @@
+package ca.lccc.myCommunityApp.entity;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 商品
+ */
+public class Product {
+    private Long productId;
+    private String productName;
+    private String productDesc;
+    // 简略图、缩列图
+    private String imgAddr;
+    private String normalPrice;
+    private String promotionPrice;
+    // 权重，越大越靠前显示
+    private Integer priority;
+    // 商品积分
+    private Integer point;
+    private Date createTime;
+    private Date lastEditTime;
+    // 0.下架 1.在前端展示系统中展示
+    private Integer enableStatus;
+    // 图片详情图列表，对商品为多对一的映射关系（n vs 1）
+    private List<ProductImg> productImgList;
+    // 商品类别，意见商品仅属于一个商品类别
+    private ProductCategory produceCategory;
+    // 店铺实体类，标明商品属于哪个店铺
+    private Shop shop;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public String getImgAddr() {
+        return imgAddr;
+    }
+
+    public void setImgAddr(String imgAddr) {
+        this.imgAddr = imgAddr;
+    }
+
+    public String getNormalPrice() {
+        return normalPrice;
+    }
+
+    public void setNormalPrice(String normalPrice) {
+        this.normalPrice = normalPrice;
+    }
+
+    public String getPromotionPrice() {
+        return promotionPrice;
+    }
+
+    public void setPromotionPrice(String promotionPrice) {
+        this.promotionPrice = promotionPrice;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public Integer getEnableStatus() {
+        return enableStatus;
+    }
+
+    public void setEnableStatus(Integer enableStatus) {
+        this.enableStatus = enableStatus;
+    }
+
+    public List<ProductImg> getProductImgList() {
+        return productImgList;
+    }
+
+    public void setProductImgList(List<ProductImg> productImgList) {
+        this.productImgList = productImgList;
+    }
+
+    public ProductCategory getProduceCategory() {
+        return produceCategory;
+    }
+
+    public void setProduceCategory(ProductCategory produceCategory) {
+        this.produceCategory = produceCategory;
+    }
+
+    public Shop getShop() {
+        return shop;
+    }
+
+    public void setShop(Shop shop) {
+        this.shop = shop;
+    }
+}
