31e018f3e6f76860f6279e21b5f0e181fd9bcce7
changchanglccc <jennifer06091992@gmail.com>
2024-03-25 10:49:45 -0400

    2.added logback, go to CATALINA_BASE folder to check log files

--- src/main/java/ca/lccc/myCommunityApp/web/superadmin/AreaController.java
+++ src/main/java/ca/lccc/myCommunityApp/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package ca.lccc.myCommunityApp.web.superadmin;
 
 import ca.lccc.myCommunityApp.entity.Area;
 import ca.lccc.myCommunityApp.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,16 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+
+    Logger logger = LoggerFactory.getLogger(AreaController.class); // 传入类的二进制文件，创建本类独有的logger对象
     @Autowired
     private AreaService areaService;
 
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     @ResponseBody // 以json数据格式类型返回
-    private Map<String, Object> listArea(){
+    private Map<String, Object> listArea() {
+        logger.info("===start===");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap = new HashMap<String, Object>();
         List<Area> list = new ArrayList<Area>();
         try {
@@ -34,7 +40,11 @@ public class AreaController {
             e.printStackTrace();
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
+            logger.error("test error!");
         }
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]", endTime - startTime);
+        logger.info("===end===");
 
         return modelMap;
     }
