792c40f1b02c543ef542f9a63f02a4198a06fad5
changchanglccc <jennifer06091992@gmail.com>
2024-03-26 23:20:19 -0400

    2.added other exceptions

---
+++ src/main/java/ca/lccc/myCommunityApp/exceptions/ProductOperationException.java
@@ -0,0 +1,9 @@
+package ca.lccc.myCommunityApp.exceptions;
+
+public class ProductOperationException extends RuntimeException {
+    private static final long serialVersionUID = 5076172298827469013L;
+
+    public ProductOperationException(String msg) {
+        super(msg);
+    }
+}
