b69bde9a05c9d9ea81291b4716459c57700bb1ff
changchanglccc <jennifer06091992@gmail.com>
2024-03-27 22:48:33 -0400

    2.completed registerShop in ShopManagementController

---
+++ src/main/java/ca/lccc/myCommunityApp/util/HttpServletRequestUtil.java
@@ -0,0 +1,56 @@
+package ca.lccc.myCommunityApp.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/***
+ * 对于json格式的数据，写解析成各种类型数据的方法，写到一个Util类中
+ */
+public class HttpServletRequestUtil {
+    public static int getInt(HttpServletRequest request, String key) {
+        try {
+            return Integer.decode(request.getParameter(key));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static long getLong(HttpServletRequest request, String key) {
+        try {
+            return Long.valueOf(request.getParameter(key));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static Double getDouble(HttpServletRequest request, String key) {
+        try {
+            return Double.valueOf(request.getParameter(key));
+        } catch (Exception e) {
+            return -1d;
+        }
+    }
+
+    public static boolean getBoolean(HttpServletRequest request, String key) {
+        try {
+            return Boolean.valueOf(request.getParameter(key));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static String getString(HttpServletRequest request, String key) {
+        try {
+            String result = request.getParameter(key);
+            if (result != null) {
+                result = result.trim();
+            }
+            if ("".equals(result)) {
+                result = null;
+            }
+            return result;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
+
---
+++ src/main/java/ca/lccc/myCommunityApp/web/shopadmin/ShopManagementController.java
@@ -0,0 +1,101 @@
+package ca.lccc.myCommunityApp.web.shopadmin;
+
+import ca.lccc.myCommunityApp.dto.ImageHolder;
+import ca.lccc.myCommunityApp.dto.ShopExecution;
+import ca.lccc.myCommunityApp.entity.PersonInfo;
+import ca.lccc.myCommunityApp.entity.Shop;
+import ca.lccc.myCommunityApp.enums.ShopStateEnum;
+import ca.lccc.myCommunityApp.exceptions.ShopOperationException;
+import ca.lccc.myCommunityApp.service.ShopService;
+import ca.lccc.myCommunityApp.util.HttpServletRequestUtil;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/shopadmin")
+public class ShopManagementController {
+    @Autowired
+    private ShopService shopService;
+
+    @RequestMapping("/registershop")
+    @ResponseBody
+    public Map<String, Object> registerShop(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+
+        // 1.接收并转化相应的参数，包括店铺信息以及图片信息
+        String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
+        ObjectMapper mapper = new ObjectMapper();
+        Shop shop = null;
+        try {
+            shop = mapper.readValue(shopStr, Shop.class);
+
+        } catch (Exception e) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.getMessage());
+            return modelMap;
+        }
+        CommonsMultipartFile shopImg = null;
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (commonsMultipartResolver.isMultipart(request)) {
+            MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+            shopImg = (CommonsMultipartFile) multipartHttpServletRequest.getFile("shopImg");
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "uploaded image cannot be null");
+            return modelMap;
+        }
+
+        // 2.注册店铺
+        if (shop != null && shopImg != null) {
+            //前端数据不可靠，不通过ownerId得到owner，而从session中获取（如果用户登录了，session中会有user信息）
+            //PersonInfo owner = (PersonInfo) request.getSession().getAttribute("user");
+            PersonInfo owner = new PersonInfo();
+            owner.setUserId(1L);
+            shop.setOwner(owner);
+            ShopExecution se;   //店铺状态标识
+            try {
+                ImageHolder imageHolder = new ImageHolder(shopImg.getOriginalFilename(), shopImg.getInputStream());
+                se = shopService.addShop(shop, imageHolder);
+                if (se.getState() == ShopStateEnum.CHECK.getState()) {
+                    modelMap.put("success", true);
+                    // 该用户可以操作的店铺列表
+                    @SuppressWarnings("unchecked") // 告诉编译器忽略unchecked的警告信息，如使用List,ArrayList等未进行参数化产生的警告信息
+                    List<Shop> shopList = (List<Shop>) request.getSession().getAttribute("shopList");
+                    if (shopList == null || shopList.size() == 0) {
+                        shopList = new ArrayList<Shop>();
+                    }
+                    shopList.add(se.getShop());
+                    request.getSession().setAttribute("shopList", shopList);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", se.getStateInfo());
+                }
+            } catch (ShopOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.getMessage());
+            } catch (IOException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.getMessage());
+            }
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "input shop info failed");
+        }
+        return modelMap;
+    }
+}
