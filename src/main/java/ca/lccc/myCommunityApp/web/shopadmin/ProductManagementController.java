7e23cfb0c76ef173356c6ef0cd7b2c75720172d7
changchanglccc <jennifer06091992@gmail.com>
2024-04-13 08:33:51 -0400

    4.added modifyproduct and  getproductbyid in back end
    4-1.debugged modifyproduct in both front and back end (http://localhost:8080/myCommunityApp/shopadmin/shopmanagement?shopId=1
    http://localhost:8080/myCommunityApp/shopadmin/productoperation?productId=1)

--- src/main/java/ca/lccc/myCommunityApp/web/shopadmin/ProductManagementController.java
+++ src/main/java/ca/lccc/myCommunityApp/web/shopadmin/ProductManagementController.java
@@ -3,9 +3,11 @@ package ca.lccc.myCommunityApp.web.shopadmin;
 import ca.lccc.myCommunityApp.dto.ImageHolder;
 import ca.lccc.myCommunityApp.dto.ProductExecution;
 import ca.lccc.myCommunityApp.entity.Product;
+import ca.lccc.myCommunityApp.entity.ProductCategory;
 import ca.lccc.myCommunityApp.entity.Shop;
 import ca.lccc.myCommunityApp.enums.ProductStateEnum;
 import ca.lccc.myCommunityApp.exceptions.ProductOperationException;
+import ca.lccc.myCommunityApp.service.ProductCategoryService;
 import ca.lccc.myCommunityApp.service.ProductService;
 import ca.lccc.myCommunityApp.util.CodeUtil;
 import ca.lccc.myCommunityApp.util.HttpServletRequestUtil;
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
@@ -32,7 +35,102 @@ public class ProductManagementController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
     private static final int IMAGEMAXCOUNT = 6; // 支持上传商品详情图的最大数量
+
+    /**
+     * 商品编辑
+     */
+    @RequestMapping(value = "/modifyproduct", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> modifyProduct(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        // 是商品编辑时候调用 还是 上下架操作的时候调用
+        // 若为前者则进行验证码判断，后者则跳过验证码判断
+        boolean statusChange = HttpServletRequestUtil.getBoolean(request, "statusChange");
+        // 验证码判断
+        if (!statusChange && !CodeUtil.checkVerifyCode(request)) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "error verify code");
+            return modelMap;
+        }
+        // 接收前端参数的变量的初始化，包括商品，缩略图，详情图列表实体类
+        ObjectMapper mapper = new ObjectMapper();
+        Product product = null;
+        ImageHolder thumbnail = null;
+        List<ImageHolder> productImgList = new ArrayList<ImageHolder>();
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+                request.getSession().getServletContext());
+        // 若请求中存在文件流，则取出相关的文件（包括缩略图和详情图）
+        try {
+            if (multipartResolver.isMultipart(request)) {
+                thumbnail = handleImage(request, thumbnail, productImgList);
+            }
+        } catch (Exception e) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.toString());
+            return modelMap;
+        }
+        try {
+            String productStr = HttpServletRequestUtil.getString(request, "productStr");
+            // 尝试获取前端传过来的表单string流并将其转换成Product实体类
+            product = mapper.readValue(productStr, Product.class);
+        } catch (Exception e) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.toString());
+            return modelMap;
+        }
+        // 非空判断
+        if (product != null) {
+            try {
+                // 从session中获取当前店铺的Id并赋值给product，减少对前端数据的依赖
+                Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+                product.setShop(currentShop);
+                // 开始进行商品信息变更操作
+                ProductExecution pe = productService.modifyProduct(product, thumbnail, productImgList);
+                if (pe.getState() == ProductStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", pe.getStateInfo());
+                }
+            } catch (RuntimeException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+                return modelMap;
+            }
+
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "pls enter product info");
+        }
+        return modelMap;
+    }
+
+    /**
+     * 通过商品id获取商品信息
+     */
+    @RequestMapping(value = "/getproductbyid", method = RequestMethod.GET)
+    @ResponseBody
+    private Map<String, Object> getProductById(@RequestParam Long productId) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        // 非空判断
+        if (productId > -1) {
+            // 获取商品信息
+            Product product = productService.getProductById(productId);
+            // 获取该店铺下的商品类别列表
+            List<ProductCategory> productCategoryList = productCategoryService.getProductCategoryList(product.getShop().getShopId());
+            modelMap.put("product", product);
+            modelMap.put("productCategoryList", productCategoryList);
+            modelMap.put("success", true);
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "empty productId");
+        }
+        return modelMap;
+    }
 
     @RequestMapping(value = "/addproduct", method = RequestMethod.POST)
     @ResponseBody
